d30f9b34bd09a9acef5bf4ed5a173f6e0dea861b
Maj Toan <toaney@gmail.com>
2019-12-12 10:17:31 -0800

    outline lucky number generator component

---
+++ frontend/src/components/luckyNumberGenerator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LuckyNumberGenerator = () => {
+    //pull past winning numbers
+    //pull in date info
+    //pull in time info
+
+
+
+    //randomly generate number
+
+
+
+
+
+    //output five numbers and a mega
+    //array of 5 numbers + 1 mega number
+
+    //conditions no duplicate numbers for the first 5
+    //6th number can be duplicate
+
+    //helper function to generate number
+
+
+    //helper function to generate mega
+
+
+
+
+}
